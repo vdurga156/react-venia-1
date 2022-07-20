067b3bfec90513806dd0f02eb43fd8647aca4f20
vdurga156 <105192803+vdurga156@users.noreply.github.com>
2022-07-20 13:56:37 +0530

    new commit

---
+++ src/component/Filter.js
@@ -0,0 +1,100 @@
+import React from 'react'
+import "../Style/Filter.scss"
+
+
+function Filter() {
+    return (
+         <div className='filterone'>
+                    <div className='filter'>
+                        <h6>Filters</h6>
+                    </div>
+                    <hr/>
+                    <div className='attribute'>
+                        <h6>Attribute</h6>
+                        <input type="checkbox" id="option" name="option" value="option" />
+                        <label>Option</label> <br />
+                        <input type="checkbox" id="option" name="option" value="option" />
+                        <label>Option</label> <br />
+                        <input type="checkbox" id="option" name="option" value="option" />
+                        <label>Option</label> <br />
+                        <input type="checkbox" id="option" name="option" value="option" />
+                        <label>Option</label> <br />
+                        <div className='showmor'>
+                            <a href="#show" class="">
+                                show more
+                            </a>
+                        </div>
+                        <hr />
+                    </div>
+                    <div>
+                        <h6>Attribute</h6>
+                        <input type="checkbox" id="option" name="option" value="option" />
+                        <label>Option</label> <br />
+                        <input type="checkbox" id="option" name="option" value="option" />
+                        <label>Option</label> <br />
+                        <input type="checkbox" id="option" name="option" value="option" />
+                        <label>Option</label> <br />
+                        <input type="checkbox" id="option" name="option" value="option" />
+                        <label>Option</label> <br />
+                        <input type="checkbox" id="option" name="option" value="option" />
+                        <label>Option</label> <br />
+                        <input type="checkbox" id="option" name="option" value="option" />
+                        <label>Option</label> <br />
+                        <hr />
+                    </div>
+
+
+                    <div>
+                        <div>
+                            <h6>Color</h6>
+                        </div>
+                        <div className='colorfilter'>
+                            <div class="red_div"></div><br />
+                            <div class="red_div1"></div>
+                            <div class="red_div2"></div>
+                            <div class="red_div3"></div>
+                        </div>
+                        <br />
+                        <div className='colorfilter'>
+                            <div class="red_div4"></div><br />
+                            <div class="red_div5"></div>
+                            <div class="red_div6"></div>
+                            <div class="red_div7"></div>
+                        </div>
+                        <br />
+                        <div className='colorfilter'>
+                            <div class="red_div8"></div><br />
+                            <div class="red_div9"></div>
+                            
+
+                        </div>
+
+
+                    </div>
+
+<hr/>
+                    <div>
+                        <h6>Attribute</h6>
+                        <input type="checkbox" id="option" name="option" value="option" />
+                        <label>Option</label> <br />
+                        <input type="checkbox" id="option" name="option" value="option" />
+                        <label>Option</label> <br />
+                        <input type="checkbox" id="option" name="option" value="option" />
+                        <label>Option</label> <br />
+                        <input type="checkbox" id="option" name="option" value="option" />
+                        <label>Option</label> <br />
+                        <input type="checkbox" id="option" name="option" value="option" />
+                        <label>Option</label> <br />
+                        <input type="checkbox" id="option" name="option" value="option" />
+                        <label>Option</label> <br />
+                        <input type="checkbox" id="option" name="option" value="option" />
+                        <label>Option</label> <br />
+                        <input type="checkbox" id="option" name="option" value="option" />
+                        <label>Option</label> <br />
+                        <hr />
+                    </div>
+                    </div>
+    )
+}
+
+export default Filter
